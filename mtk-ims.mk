89f9dd9b4c4148db5e6202fd849c7fec7a6eb9d7
shiki senpai <guptavishal998.pa@gmail.com>
2023-02-12 02:49:26 +0530

    ds

---
+++ mtk-ims.mk
@@ -0,0 +1,26 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/realme/IMS-denniz
+
+# Apps
+PRODUCT_PACKAGES += \
+    ImsService
+
+# Radio IMS Boot Jar
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+# Radio IMS Jars
+PRODUCT_PACKAGES += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
